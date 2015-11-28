fb5f938287a4fecde3469bece93748ad1451eaf7
unknown <fb991345@C39024.ens.uqam.ca>
2015-11-28 10:32:52 -0500

    class Log vide

---
+++ server/app/controllers/Log.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import views.html.*;
+import java.util.*;
+import models.*;
+
+public class Log extends Controller {
+
+    public Log () {
+
+    }
+
+}
