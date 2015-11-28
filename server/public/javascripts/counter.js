bbdcaa584cb3d6a77c85aa2cf1524e1bfe2def31
Yuri Stavchanskyy <yuristavchanskyy@Yuris-MacBook-Pro.local>
2015-11-28 15:52:27 -0500

    creation de timer; exigence #15

--- server/public/javascripts/counter.js
+++ server/public/javascripts/counter.js
@@ -1,5 +1,6 @@
 
         
+
         function desableSection(id){
            var fields = document.getElementById(id).getElementsByTagName('*');
                 for(var i = 0; i < fields.length; i++){
@@ -14,62 +15,121 @@
                 } 
             
         }
-        
-        function getTimeRemaining(endtime){
-            var t = Date.parse(endtime) + Date.parse(new Date());
-            var seconds = Math.floor( (t/1000) % 60 );
-            var minutes = Math.floor( (t/1000/60) % 60 );
-            var hours = Math.floor( (t/(1000*60*60)) % 24 );
-            var days = Math.floor( t/(1000*60*60*24) );
-            return {
-                'total': t,
-                'days': days,
-                'hours': hours,
-                'minutes': minutes,
-                'seconds': seconds
-            };
-        }
+             
+        var second = 0;
+        var minute = 0;
+        var hour = 0;
+        var isRunning = 0;
+        var isStarted = 0;
          
-        function initializeClock(id, endtime){
+        function initializeClock(id, number){
             var clock = document.getElementById(id);
-         //   var daysSpan = clock.querySelector('.days');
+            var daysSpan = clock.querySelector('.days');
             var hoursSpan = clock.querySelector('.hours');
             var minutesSpan = clock.querySelector('.minutes');
-         //   var secondsSpan = clock.querySelector('.seconds');
-             
-            function updateClock(){
-                var t = getTimeRemaining(endtime);
-                 
-              //  daysSpan.innerHTML = t.days;
-                hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-                minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-            //    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-                 
-                if(t.total<=0){
-                    clearInterval(timeinterval);
-                }
+            var secondsSpan = clock.querySelector('.seconds');
+           
+            hoursSpan.innerHTML = hour;
+            minutesSpan.innerHTML = minute;
+           
+            var timeinterval = setInterval(increment, number);
+        }
+        
+        var i = 1;
+        function increment(){
+            if(isRunning === 0 || isRunning === -1){
+                return;
+            }
+            second = second + 1;
+            if(second > 60){
+                second = 0;
+                minute = minute + 1;
             }
              
-            updateClock();
-            var timeinterval = setInterval(updateClock,1000);
+            if(minute > 60){
+                minute = 0;
+                hour = hour + 1;
+            }
+             
+            var clock = document.getElementById('clockdiv');
+             
+            var secondsSpan = clock.querySelector('.seconds');
+            var hoursSpan = clock.querySelector('.hours');
+            var minutesSpan = clock.querySelector('.minutes');
+            hoursSpan.innerHTML = hour;
+            minutesSpan.innerHTML = minute;
+             
+        }
+        
+        
+        function startCounter(){
+            if(isRunning === -1){
+            	return;
+            }
+            var clock = document.getElementById('clockdiv');
+            var start = clock.querySelector('#idBtnStart');
+            if(isStarted === 0){
+                initializeClock('clockdiv',1000); 
+                isStarted = 1;
+            }
+             
+            if(isStarted === 1){
+                if(isRunning === 1){
+                    isRunning = 0;
+                }else{
+                    isRunning = 1;
+                }
+            }
+                    
+        }
+        
+        function resetCounter(){
+            isRunning = 0;
+            minute = 0;
+            hour = 0;
+             
+            var clock = document.getElementById('clockdiv');
+            clock.querySelector('.resetButton');
+            var hoursSpan = clock.querySelector('.hours');
+            var minutesSpan = clock.querySelector('.minutes');
+            hoursSpan.innerHTML = 0;
+            minutesSpan.innerHTML = 0;
+        }
+        
+        
+        function visButtonShow(id){
+        	$("#" + id).removeClass('hidden');
+        }
+        
+        function visButtonHide(id){
+        	$("#" + id).addClass('hidden');
         }
          
-        var deadline = 'December 31 2015 00:00:50 UTC+0200';
-        initializeClock('clockdiv', deadline);
-        
-        
-        
-        
-//        function visButtonShow(id){
-//        	document.getElementById(id).style.visibility = 'hidden';
-//        }
-        
-//         function visButtonHide(id){
-//        	 document.getElementById(id).style.visibility = 'hidden';
-//         }
-         
     
-	
-      
+        function stopButton(id){
+        	desableSection(id);
+        	stopCounter();
+        	visButtonHide('idBtnPause');
+        	visButtonShow('idBtnStart');
+        }
+        
+        function startButton(id){
+        	enableSection(id);
+        	visButtonHide('idBtnStart');
+        	visButtonShow('idBtnPause');
+        	startCounter();	
+        }
+        
+        function pauseButton(id){
+        	startCounter();
+        	visButtonHide('idBtnPause');
+        	visButtonShow('idBtnStart');
+        	desableSection(id);
+        	
+        }
+        
+        function stopCounter(){
+            isRunning = 0;
+        }
         
 
