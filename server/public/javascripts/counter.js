6216cdb6f1b082a72b5bab5dbeeef96856b9b635
Yuri Stavchanskyy <yuristavchanskyy@Yuris-MacBook-Pro.local>
2015-11-28 14:01:32 -0500

    ajout du compteur

--- server/public/javascripts/counter.js
+++ server/public/javascripts/counter.js
@@ -32,18 +32,18 @@
          
         function initializeClock(id, endtime){
             var clock = document.getElementById(id);
-            var daysSpan = clock.querySelector('.days');
+         //   var daysSpan = clock.querySelector('.days');
             var hoursSpan = clock.querySelector('.hours');
             var minutesSpan = clock.querySelector('.minutes');
-            var secondsSpan = clock.querySelector('.seconds');
+         //   var secondsSpan = clock.querySelector('.seconds');
              
             function updateClock(){
                 var t = getTimeRemaining(endtime);
                  
-                daysSpan.innerHTML = t.days;
+              //  daysSpan.innerHTML = t.days;
                 hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
                 minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-                secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+            //    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
                  
                 if(t.total<=0){
                     clearInterval(timeinterval);
