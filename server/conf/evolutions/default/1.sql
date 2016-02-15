fc9ccba5718af83187561ac8394f404811e8e607
jimmycloutier <scrooll@hotmail.com>
2016-02-14 22:42:29 -0500

    modified:   server/conf/evolutions/default/1.sql

--- server/conf/evolutions/default/1.sql
+++ server/conf/evolutions/default/1.sql
@@ -3,6 +3,12 @@
 
 # --- !Ups
 
+create table organisation (
+  id                        integer primary key AUTOINCREMENT,
+  name                      varchar(255),
+  id_admin                  integer)
+;
+
 create table team_member (
   id                        integer primary key AUTOINCREMENT,
   name                      varchar(255) not null,
@@ -10,62 +16,40 @@ create table team_member (
   password                  varchar(255) not null)
 ;
 
-=======
-create table organisation (
-  id                        bigint not null,
-  name                      varchar(255),
-  id_admin                  bigint,
-  constraint pk_organisation primary key (id))
-;
-
 create table user (
-  id                        bigint not null,
+  id                        integer primary key AUTOINCREMENT,
   alias                     varchar(255),
   name                      varchar(255),
   password                  varchar(255),
   email                     varchar(255),
-  deleted                   boolean,
-  constraint uq_user_email unique (email),
-  constraint pk_user primary key (id))
+  deleted                   integer(1),
+  constraint uq_user_email unique (email))
 ;
 
 
 create table organisation_user (
-  organisation_id                bigint not null,
-  user_id                        bigint not null,
+  organisation_id                integer not null,
+  user_id                        integer not null,
   constraint pk_organisation_user primary key (organisation_id, user_id))
 ;
-create sequence organisation_seq;
-
-create sequence user_seq;
 
 
 
+alter table organisation_user add constraint fk_organisation_user_organisa_01 foreign key (organisation_id) references organisation (id);
 
-alter table organisation_user add constraint fk_organisation_user_organisa_01 foreign key (organisation_id) references organisation (id) on delete restrict on update restrict;
-
-alter table organisation_user add constraint fk_organisation_user_user_02 foreign key (user_id) references user (id) on delete restrict on update restrict;
+alter table organisation_user add constraint fk_organisation_user_user_02 foreign key (user_id) references user (id);
 
 # --- !Downs
 
-
 PRAGMA foreign_keys = OFF;
 
-SET REFERENTIAL_INTEGRITY FALSE;
+drop table organisation;
 
-drop table if exists organisation;
-
-drop table if exists organisation_user;
-
-drop table if exists user;
+drop table organisation_user;
 
 drop table team_member;
 
+drop table user;
 
 PRAGMA foreign_keys = ON;
-
-drop sequence if exists organisation_seq;
-
-drop sequence if exists user_seq;
-
 
