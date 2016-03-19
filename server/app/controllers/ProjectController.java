b7e239bdec29a9b9493e82826919d4756b835689
Feo <feo.pascal@ens.uqam.ca>
2016-03-19 11:41:34 -0400

    C:/Users/cj291108/AppData/Local/Programs/Git/project with id

--- server/app/controllers/ProjectController.java
+++ server/app/controllers/ProjectController.java
@@ -13,6 +13,20 @@ public class ProjectController extends Controller {
 
     public Result index(Long projectId) {
         
+        Project project;
+        
+        Form<Project> projectForm = form(Project.class);
+        
+        if(projectId != 0) {
+            project = Project.find.byId(projectId);
+            if(project == null) {
+                return ok(project.render());
+            } else {
+                return ok(project.render());
+            }
+            
+        }
+        
         return ok(project.render());
     }
     
