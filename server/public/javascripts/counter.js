2abc883eeef0cd4b740bb39e1a7a5469d40d69f1
Frédéric Vachon <fredvac@gmail.com>
2016-03-19 14:05:44 -0400

    First draft of frontend for the Chronometer

--- server/public/javascripts/counter.js
+++ server/public/javascripts/counter.js
@@ -1,135 +1,126 @@
+var chronoSeconds = 0;
+var isRunning = 0;
+var isStarted = 0;
+var chronoIntervalHandle;
 
-        
+$(document).ready(initializeClock);
+$(window).unload(stopChrono);
 
-        function desableSection(id){
-           var fields = document.getElementById(id).getElementsByTagName('*');
-                for(var i = 0; i < fields.length; i++){
-                fields[i].disabled = true;
-                } 
-        }
-        
-        function enableSection(id){
-              var fields = document.getElementById(id).getElementsByTagName('*');
-                for(var i = 0; i < fields.length; i++){
-                fields[i].disabled = false;
-                } 
-            
-        }
-             
-        var second = 0;
-        var minute = 0;
-        var hour = 0;
-        var isRunning = 0;
-        var isStarted = 0;
-         
-        function initializeClock(id, number){
-            var clock = document.getElementById(id);
-            var daysSpan = clock.querySelector('.days');
-            var hoursSpan = clock.querySelector('.hours');
-            var minutesSpan = clock.querySelector('.minutes');
-            var secondsSpan = clock.querySelector('.seconds');
-           
-            hoursSpan.innerHTML = hour;
-            minutesSpan.innerHTML = minute;
-           
-            var timeinterval = setInterval(increment, number);
-        }
-        
-        var i = 1;
-        function increment(){
-            if(isRunning === 0 || isRunning === -1){
-                return;
-            }
-            second = second + 1;
-            if(second > 60){
-                second = 0;
-                minute = minute + 1;
-            }
-             
-            if(minute > 60){
-                minute = 0;
-                hour = hour + 1;
-            }
-             
-            var clock = document.getElementById('clockdiv');
-             
-            var secondsSpan = clock.querySelector('.seconds');
-            var hoursSpan = clock.querySelector('.hours');
-            var minutesSpan = clock.querySelector('.minutes');
-            hoursSpan.innerHTML = hour;
-            minutesSpan.innerHTML = minute;
-             
-        }
-        
-        
-        function startCounter(){
-            if(isRunning === -1){
-            	return;
-            }
-            var clock = document.getElementById('clockdiv');
-            var start = clock.querySelector('#idBtnStart');
-            if(isStarted === 0){
-                initializeClock('clockdiv',1000); 
-                isStarted = 1;
-            }
-             
-            if(isStarted === 1){
-                if(isRunning === 1){
-                    isRunning = 0;
-                }else{
-                    isRunning = 1;
-                }
-            }
-                    
-        }
-        
-        function resetCounter(){
-            isRunning = 0;
-            minute = 0;
-            hour = 0;
-             
-            var clock = document.getElementById('clockdiv');
-            clock.querySelector('.resetButton');
-            var hoursSpan = clock.querySelector('.hours');
-            var minutesSpan = clock.querySelector('.minutes');
-            hoursSpan.innerHTML = 0;
-            minutesSpan.innerHTML = 0;
-        }
-        
-        
-        function visButtonShow(id){
-        	$("#" + id).removeClass('hidden');
-        }
-        
-        function visButtonHide(id){
-        	$("#" + id).addClass('hidden');
-        }
-         
-    
-        function stopButton(id){
-        	desableSection(id);
-        	stopCounter();
-        	visButtonHide('idBtnPause');
-        	visButtonShow('idBtnStart');
-        }
-        
-        function startButton(id){
-        	enableSection(id);
-        	visButtonHide('idBtnStart');
-        	visButtonShow('idBtnPause');
-        	startCounter();	
-        }
-        
-        function pauseButton(id){
-        	startCounter();
-        	visButtonHide('idBtnPause');
-        	visButtonShow('idBtnStart');
-        	desableSection(id);
-        	
-        }
-        
-        function stopCounter(){
-            isRunning = 0;
-        }
-        
+function disableSection(id){
+    $("#" + id + " :input").prop("disabled", true);
+}
 
+function enableSection(id){
+    $("#" + id + " :input").prop("disabled", false);
+}
+
+function convertSeconds(seconds) {
+    var hours = parseInt(seconds / 3600);
+    var remainder = seconds % 3600;
+    var minutes = parseInt(remainder / 60);
+    remainder = remainder % 60;
+    var seconds = remainder;
+
+    return {
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    };
+}
+
+function initializeClock(idClock, idProject, idOrg, number){
+    var endpoint = "/organization/" + idOrg + "/project/" + idProject + "/timer";
+    var chronoSeconds = 0;
+    //     $.ajax({
+    //         url: endpoint,
+    //         data: "",
+    //         success: function(data) {
+    //             chronoSeconds = data;
+    //         },
+    //         dataType: "json"}
+    //     );
+
+    chronoSeconds = 3668;
+    setChronoTime(chronoSeconds);
+    startChrono();
+}
+
+function setChronoTime(seconds) {
+    chronoSeconds = seconds;
+    var chronoTime = convertSeconds(seconds);
+
+    $("#idHour").text(("0" + chronoTime.hours).slice(-2));
+    $("#idMin").text(("0" + chronoTime.minutes).slice(-2));
+    $("#idSec").text(("0" + chronoTime.seconds).slice(-2));
+}
+
+function startChrono() {
+    chronoIntervalHandle = setInterval(updateChronoTime, 1000);
+    console.log(chronoIntervalHandle);
+}
+
+function updateChronoTime() {
+    setChronoTime(++chronoSeconds);
+}
+
+function stopChrono() {
+    clearInterval(chronoIntervalHandle);
+}
+
+function increment(){
+    if (isRunning === 0 || isRunning === -1){
+        return;
+    }
+    second = second + 1;
+
+    if (second > 60){
+        second = 0;
+        minute = minute + 1;
+    }
+
+    if (minute > 60){
+        minute = 0;
+        hour = hour + 1;
+    }
+
+    var clock = document.getElementById('clockdiv');
+
+    var secondsSpan = clock.querySelector('.seconds');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+
+    hoursSpan.innerHTML = hour;
+    minutesSpan.innerHTML = minute;
+}
+
+function togglePlayPause() {
+    if ($("#idBtnStart").is(':visible')) {
+        // Starting timer
+        initializeClock();
+        // TODO: Replace inputTest by the form id
+        enableSection("inputTest");
+        $("#idBtnStart").hide();
+        $("#idBtnPause").show();
+        $("#clockCover").hide();
+    } else {
+        // Pausing timer
+        stopChrono();
+        // TODO: Replace inputTest by the form id
+        disableSection("inputTest");
+        $("#idBtnStart").show();
+        $("#idBtnPause").hide();
+        $("#clockCover").show();
+    }
+}
+
+function toggleSecondes() {
+    if ($(".secondes").is(':visible')) {
+        $(".secondes").hide();
+        $("#minutesDiv").attr('class','timeDiv rightDiv');
+        $("#secondesDiv").attr('class','timeDiv secondes');
+    } else {
+        $(".secondes").css('display','inline-block');
+        $("#minutesDiv").attr('class','timeDiv');
+        $("#secondesDiv").attr('class','timeDiv rightDiv secondes');
+    }
+}
