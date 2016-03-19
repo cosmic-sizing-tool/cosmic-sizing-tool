e1521f6830ea9b24ee606971f715522d3d31e32b
Feo <feo.pascal@ens.uqam.ca>
2016-03-19 10:45:28 -0400

    project page and db functional

---
+++ server/app/controllers/ProjectController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import play.*;
+import play.data.*;
+import play.mvc.*;
+import views.html.*;
+import java.util.*;
+import models.*;
+import play.data.Form;
+import static play.data.Form.*;
+
+public class ProjectController extends Controller {
+
+    public Result index(Long projectId) {
+        
+        return ok(project.render());
+    }
+    
+    
+}
+
+
