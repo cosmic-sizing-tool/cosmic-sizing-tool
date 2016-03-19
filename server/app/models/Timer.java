a1ab4e5aba8e7708b5bd566f30873c4184785c4d
Marc-André Tremblay <marcandre.tr@gmail.com>
2016-03-19 14:48:43 -0400

    Added `runningTimeInSeconds` property to serialized chrono.
    Signed-off-by: Marc-André Tremblay <marcandre.tr@gmail.com>

--- server/app/models/Timer.java
+++ server/app/models/Timer.java
@@ -5,6 +5,7 @@ import com.avaje.ebean.Expr;
 import com.avaje.ebean.Model;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import play.data.validation.Constraints;
 
 import javax.persistence.Column;
@@ -39,9 +40,57 @@ public class Timer extends Model {
     }
 
     @JsonIgnore
+    public static Timer getRunningTimer(Long organizationId, Long projectId) {
+        return Ebean
+                .find(Timer.class)
+                .where()
+                .eq("endTime", null)
+                .eq("organizationId", organizationId)
+                .eq("projectId", projectId)
+                .findUnique();
+
+    }
+
+    @JsonProperty("runningTimeInSeconds")
+    public Long timerRunningTime() {
+        Date referenceEndDate;
+        if (this.endTime == null) {
+            referenceEndDate = new Date();
+        }
+        else {
+            referenceEndDate = this.endTime;
+        }
+
+        return referenceEndDate.getTime() - this.startTime.getTime();
+    }
+
+    @JsonIgnore
     public static Timer startTimer(Long organizationId, Long projectId) {
 
-        Timer currentTimer =
+        Timer currentTimer = getRunningTimer(organizationId, projectId);
+
+        if (currentTimer == null) {
+            // We create a new timer because there is no running timer
+
+            currentTimer = new Timer();
+
+            currentTimer.organizationId = organizationId;
+            currentTimer.projectId = projectId;
+            currentTimer.startTime = new Date();
+            currentTimer.endTime = null;
+
+            currentTimer.save();
+
+        }
+
+        System.err.println(currentTimer);
+        return currentTimer;
+    }
+
+    @JsonIgnore
+    public static Timer stopTimer(Long organizationId, Long projectId) {
+
+        Timer currentTimer = getRunningTimer(organizationId, projectId);
                 Ebean
                         .find(Timer.class)
                         .where()
@@ -78,7 +127,7 @@ public class Timer extends Model {
         return timeSpent;
     }
 
-        public static List<Timer> getAllTimersForProject(Long organizationId, Long projectId) {
+    public static List<Timer> getAllTimersForProject(Long organizationId, Long projectId) {
         return Ebean.find(Timer.class)
                 .where()
                 .and(Expr.eq("organizationId", organizationId), Expr.eq("projectId", projectId))
