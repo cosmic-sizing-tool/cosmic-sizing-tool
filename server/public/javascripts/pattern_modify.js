b6da2c46ec05c1144eaa5290dd2445cd0a1e0ae2
bernardsaucier <bernard.saucier@hotmail.com>
2016-06-10 12:24:29 -0400

    Changes to get closer to database model, added size of each type of CFP and date created

--- server/public/javascripts/pattern_modify.js
+++ server/public/javascripts/pattern_modify.js
@@ -12,35 +12,66 @@ function displayPatternSize(size) {
   $("#patternSize").text(size);
 }
 
+function displayPatternSizeE(size) {
+  $("#patternSizeE").text(size);
+}
+
+function displayPatternSizeX(size) {
+  $("#patternSizeX").text(size);
+}
+
+function displayPatternSizeR(size) {
+  $("#patternSizeR").text(size);
+}
+
+function displayPatternSizeW(size) {
+  $("#patternSizeW").text(size);
+}
+
+function displayPatternDateCreated(date) {
+  $("#patternDateCreated").text(date);
+}
+
 function displayPatternName(name) {
-  $("#patternNameInput").val(name);
+  $("#Pattern_Name").val(name);
 }
 
 function displayPatternDescription(desc) {
-  $("#patternDescriptionInput").val(desc);
+  $("#Pattern_Desc").val(desc);
 }
 
 function displayPatternUser(user) {
-  $("#patternUserInput").val(user);
+  $("#User_ID").val(user);
 }
 
 function bindButton() {
   $("#patternFooter button").on("click", function() {
-    updatePattern();
+    savePattern();
   })
 }
 
-function updatePattern() {
-  if (validatePattern()) {
-  	var updatedPattern = {};
-    $("input").each(function() {
-      var id = $(this).attr("id");
-      var val = $(this).val();
-      updatedPattern[id] = val;
-    })
-    console.log("updatedPattern : ");
-    console.log(updatedPattern);
-  }
+function displayPattern(pattern) {
+  displayPatternFp(pattern.patternNFp);
+  displayPatternSize(pattern.patternSize);
+  displayPatternSizeE(pattern.patternSizeE);
+  displayPatternSizeX(pattern.patternSizeX);
+  displayPatternSizeR(pattern.patternSizeR);
+  displayPatternSizeW(pattern.patternSizeW);
+  displayPatternDateCreated(pattern.dateCreated);
+  displayPatternName(pattern.Pattern_Name);
+  displayPatternDescription(pattern.Pattern_Desc);
+  displayPatternUser(pattern.User_ID);
+}
+
+function savePattern() {
+  var isAValidPattern = validatePattern();
+  var updatedPattern = {};
+  $("#patternModify input").each(function() {
+    var id = $(this).attr("id");
+    var val = $(this).val();
+    updatedPattern[id] = val;
+  })
+  console.log("updatedPattern : ", updatedPattern);
 }
 
 function validatePattern() {
@@ -48,19 +79,36 @@ function validatePattern() {
 }
 
 
-/* Test, until there's a database */
+
+/* HARD-CODED TEST PATTERN */
+/* PatternNFp sera calculé dynamiquement à partir du nombre de FP */
+/* PatternSize sera calculé dynamiquement à partir du nombre de CFP */
+/* PatternSizeE sera calculé dynamiquement à partir du nombre de CFP de type E */
+/* PatternSizeX sera calculé dynamiquement à partir du nombre de CFP de type X */
+/* PatternSizeR sera calculé dynamiquement à partir du nombre de CFP de type R */
+/* PatternSizeW sera calculé dynamiquement à partir du nombre de CFP de type W */
 var testPatternObj = {
   patternNFp: 1,
   patternSize: 3,
-  patternNameInput: "CRUDL-3",
-  patternDescriptionInput: "Crud+List w/ 3 variables",
-  patternUserInput: "STrudel",
-}
+  patternSizeE: 1,
+  patternSizeX: 1,
+  patternSizeR: 1,
+  patternSizeW: 0,
 
-function testPattern() {
-  displayPatternFp(testPatternObj.patternNFp);
-  displayPatternSize(testPatternObj.patternSize);
-  displayPatternName(testPatternObj.patternNameInput);
-  displayPatternDescription(testPatternObj.patternDescriptionInput);
-  displayPatternUser(testPatternObj.patternUserInput);
+  Pattern_Name: "CRUDL-3",
+  Pattern_Desc: "Crud+List w/ 3 variables",
+  User_ID: "STrudel",
+  isAValidPattern: true,
+  dateCreated: new Date(),
+
+  Pattern_FP: [{
+    Pattern_ID: "",
+    Patterh_FP_ID: "",
+    Patterh_DGDM: [{
+      Pattern_FP_ID: "",
+      Pattern_DG: "",
+      Pattern_DM: 0,
+      Pattern_Size: 0,
+    }]
+  }],
 }
