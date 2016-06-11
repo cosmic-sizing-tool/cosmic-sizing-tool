cba958bd25efac5ac2feb625a2f494d5a77d8b8e
ledrou_83 <simondrouin4@gmail.com>
2016-06-11 10:57:09 -0400

    first draft on routing

--- app/controllers/Measurers.java
+++ app/controllers/Measurers.java
@@ -5,4 +5,11 @@ package controllers;
  */
 public class Measurers extends Controller {
 
+  public Result index() {
+      return ok("index!");
+  }
+
+  public Result search() {
+    return ok("search!")
+  }
 }
