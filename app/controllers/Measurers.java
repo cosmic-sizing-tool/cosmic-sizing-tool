d79647f7b142b3b25ccf1255c9da4806ff5bc8d4
Mathieu Mathurin <mathurin05@gmail.com>
2016-06-11 10:59:19 -0400

    added import

--- app/controllers/Measurers.java
+++ app/controllers/Measurers.java
@@ -3,6 +3,22 @@ package controllers;
 /**
  * Created by Mathieu on 6/11/2016.
  */
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import models.Organisation;
+import models.CosmicUser;
+//import models.User;
+import play.*;
+import play.db.ebean.Transactional;
+import play.libs.Json;
+import play.mvc.*;
+import views.html.*;
+
+import com.avaje.ebean.*;
+
 public class Measurers extends Controller {
 
   public Result index() {
