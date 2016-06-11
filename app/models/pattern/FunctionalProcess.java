ce9988776e44698c34e11bbd6376028a188263a4
Patrick Laughrea <p.laughrea@gmail.com>
2016-06-11 14:12:22 -0400

    Create FunctionalProcess.java

---
+++ app/models/pattern/FunctionalProcess.java
@@ -0,0 +1,34 @@
+/** Team Epique
+    2016/06/11 */
+
+package models.pattern;
+
+import com.avaje.ebean.Model;
+
+import java.util.*;
+
+public class FunctionalProcess extends Model {
+    private Long id;
+    private String name;
+    
+    private Set<DataGroup> dataGroupSet;
+
+    public FunctionalProcess() {
+        id = null;
+        this.dataGroupSet = new HashSet<>();
+    }
+
+    public Set<DataGroup> getDataGroupSet() {
+        return dataGroupSet;
+    }
+
+    public void setDataGroups(Set<DataGroup> dataGroupSet) {
+        this.dataGroupSet = dataGroupSet;
+    }
+    
+    public int getCFPSize() {
+        for(FunctionalProcess funcProcess : dataGroups) {
+            funcProcessSet
+        }
+    }
+}
