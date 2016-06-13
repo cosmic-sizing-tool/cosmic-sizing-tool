3322997ff4ba8c0f6125ababbd3dfbe3925a2b21
Mathy Scott <mathy.scott@gsoft.com>
2016-06-12 22:23:42 -0400

    Refactor and comments

--- public/javascripts/measurers/search.js
+++ public/javascripts/measurers/search.js
@@ -14,9 +14,10 @@ angular
             var SearchServiceRoute = '/measurers/search';
 
             //TODO: Bind country and regions on DB
+            // Countries have a region with code "" that defines the select's default option. For US it is "All States"
             var hardcodedCountryRegionJson = {"":{"name":"All Countries", "divisions":{"":"Regions"}},"CA":{"name":"Canada","divisions":{"":"All Provinces", "CA-AB":"Alberta","CA-BC":"British Columbia","CA-MB":"Manitoba","CA-NB":"New Brunswick","CA-NF":"Newfoundland","CA-NS":"Nova Scotia","CA-ON":"Ontario","CA-PE":"Prince Edward Island","CA-QC":"Quebec","CA-SK":"Saskatchewan","CA-NT":"Northwest Territories","CA-YT":"Yukon Territory"}}, "US":{"name":"United States","divisions":{"":"All States","US-AL":"Alabama","US-AK":"Alaska","US-AZ":"Arizona","US-AR":"Arkansas","US-CA":"California","US-CO":"Colorado","US-CT":"Connecticut","US-DE":"Delaware","US-FL":"Florida","US-GA":"Georgia","US-HI":"Hawaii","US-ID":"Idaho","US-IL":"Illinois","US-IN":"Indiana","US-IA":"Iowa","US-KS":"Kansas","US-KY":"Kentucky","US-LA":"Louisiana","US-ME":"Maine","US-MD":"Maryland","US-MA":"Massachusetts","US-MI":"Michigan","US-MN":"Minnesota","US-MS":"Mississippi","US-MO":"Missouri","US-MT":"Montana","US-NE":"Nebraska","US-NV":"Nevada","US-NH":"New Hampshire","US-NJ":"New Jersey","US-NM":"New Mexico","US-NY":"New York","US-NC":"North Carolina","US-ND":"North Dakota","US-OH":"Ohio","US-OK":"Oklahoma","US-OR":"Oregon","US-PA":"Pennsylvania","US-RI":"Rhode Island","US-SC":"South Carolina","US-SD":"South Dakota","US-TN":"Tennessee","US-TX":"Texas","US-UT":"Utah","US-VT":"Vermont","US-VA":"Virginia","US-WA":"Washington","US-WV":"West Virginia","US-WI":"Wisconsin","US-WY":"Wyoming","US-DC":"District of Columbia"}}};
-            var hardcodedRegionNamesJson = {"CA":"All Provinces", "US": "All States"};
 
+            //The sorting makes sure to keep the entry with code == "" on top
             function countriesFromJSON(json){
                 var countriesKeyValuePairs = _.toPairs(json);
                 var countries = _.map(countriesKeyValuePairs, function(pair){return {code: pair[0], name: pair[1].name, regions: regionsFromJSON(pair[1].divisions, pair[0])}});
@@ -28,6 +29,7 @@ angular
                 });
             };
 
+            //The sorting makes sure to keep the entry with code == "" on top
             function regionsFromJSON(json, country){
                 var regionKeyValuePairs = _.toPairs(json);
                 var regions = _.map(regionKeyValuePairs, function(pair){
